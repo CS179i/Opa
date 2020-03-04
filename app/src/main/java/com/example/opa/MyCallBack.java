74df7e52e8de5abb9f27181b6f75f93b0cc3c0ec
emmabernicerivera <erive011@ucr.edu>
2020-03-04 15:38:32 -0800

    Async Err:search for event code

---
+++ app/src/main/java/com/example/opa/MyCallBack.java
@@ -0,0 +1,5 @@
+package com.example.opa;
+
+public interface MyCallBack {
+    void onCallback(boolean value);
+}
--- app/src/main/java/com/example/opa/login_register/EventCodeActivity.java
+++ app/src/main/java/com/example/opa/login_register/EventCodeActivity.java
@@ -9,20 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.opa.MyCallBack;
 import com.example.opa.R;
-import com.example.opa.models.User;
 import com.example.opa.questionnaire.QuestionnaireActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventCodeActivity extends AppCompatActivity {
@@ -46,10 +42,25 @@ public class EventCodeActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (checkValidEventId(etEventId.getText().toString())) {
+                checkValidEventId(etEventId.getText().toString());
+            }
+        });
+    }
+
+    private void enterEvent() {
+        Intent intent = new Intent(EventCodeActivity.this, QuestionnaireActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void checkValidEventId(final String searchText) {
+        readData(new MyCallBack() {
+            @Override
+            public void onCallback(boolean value) {
+                Log.d("TAG", "value");
+                if (value) {
                     enterEvent();
-                }
-                else{
+                } else {
                     new AlertDialog.Builder(context)
                             .setTitle("Event Code Error")
                             .setMessage("Please make sure to enter a valid event code.")
@@ -57,30 +68,19 @@ public class EventCodeActivity extends AppCompatActivity {
                             .show();
                 }
             }
-        });
+        }, searchText);
     }
 
-    private void enterEvent() {
-        // TODO: add user to event in database
-        Intent intent = new Intent(EventCodeActivity.this, QuestionnaireActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    private boolean checkValidEventId(final String searchText) {
-        final Query query = mDatabase.child("events").orderByChild("eventID").equalTo(searchText);
-        query.addValueEventListener(new ValueEventListener() {
+    public void readData(final MyCallBack myCallback, final String searchtext) {
+        mDatabase.child("events").child("eventId").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.getValue() == searchText) {
-                    found = true;
-                }
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                myCallback.onCallback(value == searchtext);
             }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
         });
-        return found;
     }
 }
